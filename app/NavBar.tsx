96eca2c0609e66f7354751969b0f8eb0663ed721
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-07 00:37:55 +0600

    Updated navigation bar ui design using Shadcn

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,15 @@ import { usePathname } from "next/navigation";
 import classnames from "classnames";
 import React from "react";
 import { ModeToggle } from "@/components/mode-toggle";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -15,41 +24,52 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex justify-between border-b border-gray-200 items-center mb-5 px-5 h-16 backdrop-filter backdrop-blur-lg bg-opacity-30 sticky top-0 z-10 bg-white">
-      <div className="flex space-x-6 h-full items-center">
-        <Link href="/" className="flex space-x-3 items-center">
-          <Image
-            src="/logo.png"
-            width={50}
-            height={50}
-            alt="Bug Sculptor Logo"
-          ></Image>
-          <span className="text-3xl font-bold">Bug Sculptor</span>
-        </Link>
-        <ul className="flex space-x-6">
-          {links.map((link) => (
-            <li key={link.href}>
-              <Link
-                className={classnames({
-                  "text-zinc-900 font-bold border-b-4":
-                    link.href === currentPath,
-                  "text-zinc-500": link.href !== currentPath,
-                  "hover:text-zinc-800 transition-colors": true,
-                })}
-                href={link.href}
+    <NavigationMenu className="flex justify-between w-screen border-b border-gray-200 items-center mb-5 px-5 h-16 backdrop-filter backdrop-blur-lg bg-opacity-30 sticky top-0 z-10 bg-white">
+      <NavigationMenuList className="space-x-3">
+        <NavigationMenuItem>
+          <Link href="/" className="flex space-x-3 items-center">
+            <Image
+              src="/logo.png"
+              width={50}
+              height={50}
+              alt="Bug Sculptor Logo"
+            ></Image>
+            <span className="text-3xl font-bold">Bug Sculptor</span>
+          </Link>
+        </NavigationMenuItem>
+        {links.map((link) => (
+          <NavigationMenuItem key={link.href}>
+            <Link
+              legacyBehavior
+              passHref
+              href={link.href}
+              className={classnames({
+                "font-bold": link.href === currentPath,
+              })}
+            >
+              <NavigationMenuLink
+                className={`${navigationMenuTriggerStyle()} ${classnames({
+                  "border-b-4": link.href === currentPath,
+                })}`}
               >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex space-x-6 h-full items-center">
-        <li>
+                <span
+                  className={classnames({
+                    "font-bold": link.href === currentPath,
+                  })}
+                >
+                  {link.label}
+                </span>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+      <NavigationMenuList className="space-x-3">
+        <NavigationMenuItem>
           <ModeToggle />
-        </li>
-      </div>
-    </nav>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 

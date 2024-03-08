3e0ee70c19e6e796fcfc705700010e983c436d0d
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-08 11:55:51 +0600

    Added error alerts for handled errors on the UI

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -18,36 +20,50 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="title">Title</Label>
-        <Input
-          type="text"
-          id="title"
-          placeholder="Title"
-          {...register("title")}
-        />
-      </div>
-      <div className="grid w-full gap-1.5">
-        <Label htmlFor="description">Your description</Label>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Type your description here." {...field} />
-          )}
-        />
-      </div>
-      <Button>Submit</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Alert variant="destructive" className="mb-5">
+          <ExclamationTriangleIcon className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="title">Title</Label>
+          <Input
+            type="text"
+            id="title"
+            placeholder="Title"
+            {...register("title")}
+          />
+        </div>
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="description">Your description</Label>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Type your description here." {...field} />
+            )}
+          />
+        </div>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 

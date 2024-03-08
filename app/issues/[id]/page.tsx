4d6a8b6529ea532e390ce8a078eeeac2324d9e33
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-08 17:41:38 +0600

    Styled the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   // Redirect users to the "Not Found" page for invalid query parameters for the ID
-  if (typeof params.id !== "number") notFound();
+  if (isNaN(parseInt(params.id))) notFound();
 
   // Get the issue from the database the ID of which has been
   // specified in the URL parameters to hit this page.
@@ -19,10 +27,18 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Card>
+        <CardHeader>
+          <CardTitle>{issue.title}</CardTitle>
+          <CardDescription className="flex space-x-3 text-sm my-2">
+            <IssueStatusBadge status={issue.status} />
+            <p>{issue.createdAt.toDateString()}</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{issue.description}</p>
+        </CardContent>
+      </Card>
     </div>
   );
 };

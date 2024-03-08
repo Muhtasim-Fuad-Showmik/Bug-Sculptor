a03ec4680120f1bb3f4cd81634f07a23793ec408
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-08 16:51:37 +0600

    Added a skeleton loader to the page for loading issues

---
+++ app/issues/loading.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import IssueToolbar from "./IssueToolbar";
+
+const loading = () => {
+  const issues = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      <IssueToolbar />
+      <Table>
+        <TableCaption>A list of your tracked issues.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[300px]">Issue</TableHead>
+            <TableHead className="hidden md:table-cell">Status</TableHead>
+            <TableHead className="hidden md:table-cell">Created At</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {issues.map((issue) => (
+            <TableRow key={issue}>
+              <TableCell className="font-medium">
+                <Skeleton />
+                <div className="block md:hidden">
+                  <Skeleton />
+                </div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <Skeleton />
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <Skeleton />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default loading;

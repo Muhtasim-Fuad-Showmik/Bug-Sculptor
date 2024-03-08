24347ca53a86c59852a1109b082005178364739c
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-08 11:16:19 +0600

    Used react hook form and axios to handle form submission for creation of a new issue via the UI

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,20 +3,48 @@
 import React from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <form className="space-y-3">
+    <form
+      className="space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="title">Title</Label>
-        <Input type="text" id="title" placeholder="Title" />
+        <Input
+          type="text"
+          id="title"
+          placeholder="Title"
+          {...register("title")}
+        />
       </div>
       <div className="grid w-full gap-1.5">
         <Label htmlFor="description">Your description</Label>
-        <SimpleMDE placeholder="Type your description here." id="description" />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Type your description here." {...field} />
+          )}
+        />
       </div>
       <Button>Submit</Button>
     </form>

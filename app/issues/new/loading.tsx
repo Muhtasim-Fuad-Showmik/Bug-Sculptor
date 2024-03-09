1fd3e730ec45c14bbb051c43194763af7129d6ca
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-09 11:21:41 +0600

    Updated the loading page for creating new issues

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="max-w-xl">
+      <form className="space-y-3">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Skeleton />
+          <Skeleton />
+        </div>
+        <div className="grid w-full gap-1.5">
+          <Skeleton />
+          <Skeleton height="405px" />
+        </div>
+        <Button className="space-x-3">
+          <span>Submit</span>
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoadingNewIssuePage;

7846a08e309b610a45250474150fbba0017052e5
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-09 16:21:19 +0600

    Created a 2 column layout with an edit button in the second for the issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -9,6 +10,8 @@ import {
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   // Redirect users to the "Not Found" page for invalid query parameters for the ID
@@ -26,19 +29,27 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   if (!issue) notFound();
 
   return (
-    <div className="max-w-3xl">
-      <Card>
-        <CardHeader>
-          <CardTitle>{issue.title}</CardTitle>
-          <CardDescription className="flex space-x-3 text-sm my-2">
-            <IssueStatusBadge status={issue.status} />
-            <div>{issue.createdAt.toDateString()}</div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="prose max-w-full">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </CardContent>
-      </Card>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="max-w-3xl">
+        <Card>
+          <CardHeader>
+            <CardTitle>{issue.title}</CardTitle>
+            <CardDescription className="flex space-x-3 text-sm my-2">
+              <IssueStatusBadge status={issue.status} />
+              <div>{issue.createdAt.toDateString()}</div>
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="prose max-w-full">
+            <ReactMarkdown>{issue.description}</ReactMarkdown>
+          </CardContent>
+        </Card>
+      </div>
+      <div>
+        <Button className="space-x-1">
+          <Pencil1Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit</Link>
+        </Button>
+      </div>
     </div>
   );
 };

b3c77460dc85186802923ff1be2c7ab203b9bd7b
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-09 11:13:40 +0600

    Updated the issue detail page with skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const LoadingIssueDetailsPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="max-w-3xl">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <Skeleton />
+          </CardTitle>
+          <CardDescription className="flex space-x-3 text-sm my-2">
+            <Skeleton width="5rem" />
+            <Skeleton width="8rem" />
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Skeleton count={3} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default LoadingIssueDetailsPage;
